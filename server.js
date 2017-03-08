61aeb4946b2e04d414ffa006a5a27da20791c8fd
Scott McCord <scott.sdm27@gmail.com>
2017-03-07 20:22:04 -0500

    index route created

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const logger     = require('morgan');
 const path       = require('path');
 const bodyParser = require('body-parser');
 
+const indexRoute = require('./routes/index');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use('/', indexRoute);
+
 
 app.listen(PORT, () => console.log('The liths await on port', PORT));
