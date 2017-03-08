c105d1d35acbcfd431c964ebb8547a8f9b052a9b
Scott McCord <scott.sdm27@gmail.com>
2017-03-07 20:26:59 -0500

    server / site up and running

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const indexRoute = require('./routes/index');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// set the view engine as ejs
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.use('/', indexRoute);
 
 
