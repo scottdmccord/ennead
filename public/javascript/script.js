cb0fed35281430da5ee82b19553a5baa69908277
Scott McCord <scott.sdm27@gmail.com>
2017-03-11 12:17:43 -0500

    welcome modal functional

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,1 +1,8 @@
-console.log('javasript linked');
+console.log('hello');
+
+let logo = document.querySelector('.logo');
+let welcomeModal = document.querySelector('.landing-modal');
+logo.addEventListener('click', () => {
+  console.log('hello');
+  welcomeModal.style.display = 'none';
+});
